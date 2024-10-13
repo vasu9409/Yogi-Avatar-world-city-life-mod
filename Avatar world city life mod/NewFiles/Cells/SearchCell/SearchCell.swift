a322f0ffc497fcbea361dc52d7c6b00465f789a6
Vasu Savaliya <vasusavaliya0000@gmail.com>
2024-10-13 17:53:27 +0530

    delete files

---
+++ Avatar world city life mod/NewFiles/Cells/SearchCell/SearchCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchCell.swift
+//  Avatar world city life mod
+//
+//  Created by Mac Mini on 13/10/24.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
